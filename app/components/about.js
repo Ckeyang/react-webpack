ded641640378f7399bc14b2af938a8a60e4d22ba
ckey <463650967@qq.com>
2018-05-16 15:29:00 +0800

    添加 imutable 和 antd

--- app/components/about.js
+++ app/components/about.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 import Btn from './btn/btn';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-require('../assets/main.less');
+require( '../assets/main.less' );
 export default class About extends React.Component {
-    render() {
-        const btns = [{
-            className: "a_btn", linkUrl:"/repos", btnName: "repos"
-        }, {
-            className: "a_btn", linkUrl:"/", btnName: "app"
-        }, {
-            className: "a_btn", linkUrl: "/about", btnName: "about"
-        }];
-        return (
-            <div className="productBox">
-                About<br/>
-                {btns.map((item,i)=>{
-                    return <Link key={i} to={item.linkUrl}><Btn className={item.className} onClickListener={item.onClickListener}
-                                      btnName={item.btnName}/></Link>
-                })}
-            </div>
-        );
-    }
+  render () {
+    const btns = [ {
+      className:"a_btn", linkUrl:"/repos", btnName:"repos"
+    }, {
+      className:"a_btn", linkUrl:"/", btnName:"app"
+    }, {
+      className:"a_btn", linkUrl:"/about", btnName:"about"
+    } ];
+    return (
+      <div className="productBox">
+        About<br />
+        {btns.map( ( item, i ) => {
+          return <Link key={i} to={item.linkUrl}><Btn className={item.className} onClickListener={item.onClickListener}
+                                                      btnName={item.btnName} /></Link>
+        } )}
+        <DatePicker />
+      </div>
+    );
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,104 +1,100 @@
-const path = require('path');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');//压缩插件
-const ExtractTextPlugin = require("extract-text-webpack-plugin");  //CSS样式分离成文件插件
-const HtmlwebpackPlugin = require('html-webpack-plugin');
-const Autoprefixer = require('autoprefixer');
-const config = require('./config');
-module.exports = {
-    entry: path.resolve(__dirname, './app/main.js'),
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: config.contentBase,  //设置根目录
-        host: config.host,  //主机的ip地址
-        port: config.port    //端口
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: [{loader: 'babel-loader'}]
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /(node_modules)/,
-                use: [{loader: 'babel-loader', options: {presets: ["react", "es2015"]}}]
-            },
-            {
-                test: /\.less$/,
-                exclude: /(node_modules)/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [{loader: "css-loader", options: {minimize: true}}, {
-                        loader: "less-loader",
-                        options: {noIeCompat: true, sourceMap: true}
-                    }],
-                })
-            },
-
-            {
-                test: /\.sass$/, exclude: /(node_modules)/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [{loader: "css-loader", options: {minimize: true}}, {
-                        loader: "sass-loader",
-                        options: {noIeCompat: true, sourceMap: true}
-                    }],
-                })
-            },
-            {
-                test: /\.css$/,
-                exclude: /(node_modules)/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [{loader: "css-loader", options: {minimize: true}}],
-                })
-            },
-            {
-                test: /\.(png|jpg|woff|woff2|svg|ttf|eot)($|\?)/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000
-                        }
-                    }
-                ]
-            },
-
-        ]
-    },
-    plugins: [
-        // CSS文件分离
-        new ExtractTextPlugin({
-            filename: "css/index.css"
-        }),
-        Autoprefixer,
-        new HtmlwebpackPlugin({
-            template: "index.html"
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            output: {
-                comments: false,  // remove all comments
-            },
-            compress: {
-                warnings: false
+const path              = require( 'path' );
+const webpack           = require( 'webpack' );
+const UglifyJSPlugin    = require( 'uglifyjs-webpack-plugin' );//压缩插件
+const ExtractTextPlugin = require( "extract-text-webpack-plugin" );  //CSS样式分离成文件插件
+const HtmlwebpackPlugin = require( 'html-webpack-plugin' );
+const Autoprefixer      = require( 'autoprefixer' );
+const config            = require( './config' );
+module.exports          = {
+  entry:path.resolve( __dirname, './app/main.js' ),
+  output:{
+    path:path.resolve( __dirname, './build' ),
+    filename:'bundle.js'
+  },
+  devServer:{
+    contentBase:config.contentBase,  //设置根目录
+    host:config.host,  //主机的ip地址
+    port:config.port    //端口
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude:/(node_modules)/,
+        use:[ { loader:'babel-loader' } ]
+      },
+      {
+        test:/\.jsx$/,
+        exclude:/(node_modules)/,
+        use:[ { loader:'babel-loader', options:{ presets:[ "react", "es2015" ] } } ]
+      },
+      {
+        test:/\.less$/,
+        use:ExtractTextPlugin.extract( {
+          fallback:"style-loader",
+          use:[ { loader:"css-loader", options:{ minimize:true } }, {
+            loader:"less-loader",
+            options:{ noIeCompat:true, sourceMap:true }
+          } ],
+        } )
+      },
+      {
+        test:/\.sass$/,
+        use:ExtractTextPlugin.extract( {
+          fallback:"style-loader",
+          use:[ { loader:"css-loader", options:{ minimize:true } }, {
+            loader:"sass-loader",
+            options:{ noIeCompat:true, sourceMap:true }
+          } ],
+        } )
+      },
+      {
+        test:/\.css$/,
+        use:ExtractTextPlugin.extract( {
+          fallback:"style-loader",
+          use:[ { loader:"css-loader", options:{ minimize:true } } ],
+        } )
+      },
+      {
+        test:/\.(png|jpg|woff|woff2|svg|ttf|eot)($|\?)/i,
+        use:[
+          {
+            loader:'url-loader',
+            options:{
+              limit:10000
             }
-        }),
-        new UglifyJSPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify("production")
-            },
-        })
-    ],
-    resolve: {
-        //自动扩展文件后缀名
-        extensions: ['.js', '.json', '.scss', '.ts']
-    }
+          }
+        ]
+      },
+    ]
+  },
+  plugins:[
+    // CSS文件分离
+    new ExtractTextPlugin( {
+      filename:"css/index.css"
+    } ),
+    Autoprefixer,
+    new HtmlwebpackPlugin( {
+      template:"index.html"
+    } ),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin( {
+      output:{
+        comments:false,  // remove all comments
+      },
+      compress:{
+        warnings:false
+      }
+    } ),
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin( {
+      'process.env':{
+        NODE_ENV:JSON.stringify( "production" )
+      },
+    } )
+  ],
+  resolve:{
+    //自动扩展文件后缀名
+    extensions:[ '.js', '.json', '.scss', '.ts' ]
+  }
 };
